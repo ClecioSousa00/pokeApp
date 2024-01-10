54be18949651d0277cb800e8757dbf0a6996647c
ClecioSousa00 <cleciosouza015@gmail.com>
2024-01-10 19:21:26 -0300

    feat:initial config

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { ThemeProvider } from '@shopify/restyle'
+import { theme } from './src/theme'
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
